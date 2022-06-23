dac31898be8a7648a59e0fbad910cc4c617ae17e
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2022-06-23 10:49:36 +0800

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace FinanceApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ellipseControl1 = new FinanceApp.UserInterface.Components.EllipseControl();
+            this.smoothPanel1 = new FinanceApp.UserInterface.Controls.SmoothPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.smoothButton1 = new FinanceApp.UserInterface.Controls.SmoothButton();
+            this.smoothButton2 = new FinanceApp.UserInterface.Controls.SmoothButton();
+            this.smoothButton3 = new FinanceApp.UserInterface.Controls.SmoothButton();
+            this.smoothButton4 = new FinanceApp.UserInterface.Controls.SmoothButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ellipseControl1
+            // 
+            this.ellipseControl1.CornerRadius = 30;
+            this.ellipseControl1.TargetControl = this;
+            // 
+            // smoothPanel1
+            // 
+            this.smoothPanel1.Location = new System.Drawing.Point(80, -12);
+            this.smoothPanel1.Name = "smoothPanel1";
+            this.smoothPanel1.SetFillColor = System.Drawing.Color.White;
+            this.smoothPanel1.Size = new System.Drawing.Size(1151, 781);
+            this.smoothPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FinanceApp.Properties.Resources.sdfghjk;
+            this.pictureBox1.Location = new System.Drawing.Point(18, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // smoothButton1
+            // 
+            this.smoothButton1.BackColor = System.Drawing.Color.White;
+            this.smoothButton1.BackgroundColor = System.Drawing.Color.White;
+            this.smoothButton1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.smoothButton1.BorderRadius = 10;
+            this.smoothButton1.BorderSize = 0;
+            this.smoothButton1.FlatAppearance.BorderSize = 0;
+            this.smoothButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.smoothButton1.ForeColor = System.Drawing.Color.White;
+            this.smoothButton1.Image = global::FinanceApp.Properties.Resources.home_20px;
+            this.smoothButton1.Location = new System.Drawing.Point(21, 250);
+            this.smoothButton1.Name = "smoothButton1";
+            this.smoothButton1.Size = new System.Drawing.Size(42, 42);
+            this.smoothButton1.TabIndex = 1;
+            this.smoothButton1.TextColor = System.Drawing.Color.White;
+            this.smoothButton1.UseVisualStyleBackColor = false;
+            // 
+            // smoothButton2
+            // 
+            this.smoothButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(44)))), ((int)(((byte)(170)))));
+            this.smoothButton2.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(44)))), ((int)(((byte)(170)))));
+            this.smoothButton2.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.smoothButton2.BorderRadius = 10;
+            this.smoothButton2.BorderSize = 0;
+            this.smoothButton2.FlatAppearance.BorderSize = 0;
+            this.smoothButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.smoothButton2.ForeColor = System.Drawing.Color.White;
+            this.smoothButton2.Image = global::FinanceApp.Properties.Resources.home_20px;
+            this.smoothButton2.Location = new System.Drawing.Point(21, 335);
+            this.smoothButton2.Name = "smoothButton2";
+            this.smoothButton2.Size = new System.Drawing.Size(42, 42);
+            this.smoothButton2.TabIndex = 1;
+            this.smoothButton2.TextColor = System.Drawing.Color.White;
+            this.smoothButton2.UseVisualStyleBackColor = false;
+            // 
+            // smoothButton3
+            // 
+            this.smoothButton3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(44)))), ((int)(((byte)(170)))));
+            this.smoothButton3.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(44)))), ((int)(((byte)(170)))));
+            this.smoothButton3.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.smoothButton3.BorderRadius = 10;
+            this.smoothButton3.BorderSize = 0;
+            this.smoothButton3.FlatAppearance.BorderSize = 0;
+            this.smoothButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.smoothButton3.ForeColor = System.Drawing.Color.White;
+            this.smoothButton3.Image = global::FinanceApp.Properties.Resources.home_20px;
+            this.smoothButton3.Location = new System.Drawing.Point(21, 420);
+            this.smoothButton3.Name = "smoothButton3";
+            this.smoothButton3.Size = new System.Drawing.Size(42, 42);
+            this.smoothButton3.TabIndex = 1;
+            this.smoothButton3.TextColor = System.Drawing.Color.White;
+            this.smoothButton3.UseVisualStyleBackColor = false;
+            // 
+            // smoothButton4
+            // 
+            this.smoothButton4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(44)))), ((int)(((byte)(170)))));
+            this.smoothButton4.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(44)))), ((int)(((byte)(170)))));
+            this.smoothButton4.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.smoothButton4.BorderRadius = 10;
+            this.smoothButton4.BorderSize = 0;
+            this.smoothButton4.FlatAppearance.BorderSize = 0;
+            this.smoothButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.smoothButton4.ForeColor = System.Drawing.Color.White;
+            this.smoothButton4.Image = global::FinanceApp.Properties.Resources.home_20px;
+            this.smoothButton4.Location = new System.Drawing.Point(21, 505);
+            this.smoothButton4.Name = "smoothButton4";
+            this.smoothButton4.Size = new System.Drawing.Size(42, 42);
+            this.smoothButton4.TabIndex = 1;
+            this.smoothButton4.TextColor = System.Drawing.Color.White;
+            this.smoothButton4.UseVisualStyleBackColor = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(44)))), ((int)(((byte)(170)))));
+            this.ClientSize = new System.Drawing.Size(1200, 720);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.smoothButton4);
+            this.Controls.Add(this.smoothButton3);
+            this.Controls.Add(this.smoothButton2);
+            this.Controls.Add(this.smoothButton1);
+            this.Controls.Add(this.smoothPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Main_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private UserInterface.Components.EllipseControl ellipseControl1;
+        private UserInterface.Controls.SmoothPanel smoothPanel1;
+        private UserInterface.Controls.SmoothButton smoothButton1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private UserInterface.Controls.SmoothButton smoothButton4;
+        private UserInterface.Controls.SmoothButton smoothButton3;
+        private UserInterface.Controls.SmoothButton smoothButton2;
+    }
+}
+
---
+++ UserInterface/Controls/SmoothPanel.cs
@@ -0,0 +1,61 @@
+﻿using FinanceApp.UserInterface.Classes;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinanceApp.UserInterface.Controls
+{
+    internal class SmoothPanel : Panel
+    {
+        #region Variables
+        /// <summary>
+        /// variable declarations
+        /// </summary>
+        private Color fill_color;
+        #endregion
+
+        #region access descriptors
+        public Color SetFillColor
+        {
+            get { return fill_color; }
+            set => fill_color = value;
+        }
+        #endregion
+        /// <summary>
+        /// override the paint method of the main control
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.FillRoundedRectangle(new SolidBrush(fill_color), 10, 10, this.Width - 40, this.Height - 60, 10);
+            SolidBrush brush = new SolidBrush(
+                fill_color
+                );
+            g.FillRoundedRectangle(brush, 12, 12, this.Width - 44, this.Height - 64, 10);
+            g.DrawRoundedRectangle(new Pen(ControlPaint.Light(fill_color, 0.00f)), 12, 12, this.Width - 44, this.Height - 64, 10);
+            g.FillRoundedRectangle(new SolidBrush(fill_color), 12, 12 + ((this.Height - 64) / 2), this.Width - 44, (this.Height - 64) / 2, 10);
+        }
+        protected override void OnResize(EventArgs eventargs)
+        {
+            base.OnResize(eventargs);
+            this.Invalidate(); //redraw the control
+        }
+        const int WS_EX_TRANSPARENT = 0x20;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                var cp = base.CreateParams;
+                cp.ExStyle |= WS_EX_TRANSPARENT;
+                return cp;
+            }
+        }
+    }
+}
